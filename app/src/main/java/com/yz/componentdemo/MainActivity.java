c1af77d2781e45c8697ed32fbf4867d85221595b
a200815015 <xjn-yz@163.com>
2018-06-21 18:45:51 +0800

    动态添加卸载组件

--- app/src/main/java/com/yz/componentdemo/MainActivity.java
+++ app/src/main/java/com/yz/componentdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.luojilab.component.componentlib.router.Router;
@@ -13,7 +15,7 @@ import com.yz.basicres.BaseApplication;
 import com.yz.comservice.readerbook.ReadBookService;
 
 @RouteNode(path = "/main", desc = "首页")
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     Fragment fragment;
     FragmentTransaction ft;
 
@@ -21,6 +23,10 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button installReadBookBtn = (Button) findViewById(R.id.install_share);
+        Button uninstallReadBtn = (Button) findViewById(R.id.uninstall_share);
+        installReadBookBtn.setOnClickListener(this);
+        uninstallReadBtn.setOnClickListener(this);
         showFragment();
     }
 
@@ -36,6 +42,18 @@ public class MainActivity extends BaseActivity {
             fragment = service.getReadBookFragment();
             ft = getSupportFragmentManager().beginTransaction();
             ft.add(R.id.tab_content, fragment).commitAllowingStateLoss();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.install_share:
+                Router.registerComponent("com.yz.share.applike.ShareApplike",getApplication());
+                break;
+            case R.id.uninstall_share:
+                Router.unregisterComponent("com.yz.share.applike.ShareApplike",getApplication());
+                break;
         }
     }
 
